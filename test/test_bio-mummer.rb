d9f952bb667e9f69f1a08b03fbd44cc7facedce0
Rob Syme <rob.syme@gmail.com>
2015-02-09 14:05:59 +0800

    Upgrade everything to new versions of MiniTest

--- test/test_bio-mummer.rb
+++ test/test_bio-mummer.rb
@@ -1,7 +1,8 @@
 require 'helper'
 
-class TestBioMummer < Test::Unit::TestCase
+class TestBioMummer < MiniTest::Test
   should "probably rename this file and start testing for real" do
     flunk "hey buddy, you should probably rename this file and start testing for real"
   end
 end
+
