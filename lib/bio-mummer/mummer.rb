a3178ca266f58277726bd9ecb1ac6ae4e9aba54e
Rob Syme <rob.syme@gmail.com>
2015-02-10 14:54:06 +0800

    Basic structure and tests

--- lib/bio-mummer/mummer.rb
+++ lib/bio-mummer/mummer.rb
@@ -1,3 +1,127 @@
 
 module BioMummer
+
+  class Alignment
+    attr_accessor :refname, :queryname, :refstart, :refstop, :querystart, :querystop, :strand, :distances
+
+    def initialize(refname, queryname, refstart, refstop, querystart, querystop, strand, distances)
+      @refname = refname
+      @queryname = queryname
+      @refstart = refstart
+      @refstop = refstop
+      @querystart = querystart
+      @querystop = querystop
+      @strand = strand
+      @distances = distances
+      @deltas = []
+    end
+
+    def deltas
+      if @deltas == []
+        a = @distances.each_with_object([0]) do |d, arr|
+          state = arr.last
+          if d > 0
+            @deltas += Array.new(d - 1, state)
+            @deltas.push(nil)
+            arr << state - 1
+          else
+            @deltas += Array.new(d * -1 - 1, state)
+            arr << state + 1
+          end
+        end
+        @deltas << a.last
+      end
+      return @deltas
+    end
+
+    def ref_to_query(ref_position)
+      position_in_alignment = ref_position - refstart + 1
+      querypos = nil
+      if position_in_alignment >= deltas.length
+        querypos = @querystart - 1 + position_in_alignment + deltas.last
+      elsif deltas[position_in_alignment-1]
+        querypos = @querystart - 1 + position_in_alignment + deltas[position_in_alignment-1]
+      end
+      !@strand && querypos ? @querystart + @querystop - querypos : querypos
+    end
+  end
+  
+  class DeltaFile
+    NUCMER = 1
+    PROMER = 2
+
+    attr_accessor :reference_filename, :alternate_filename
+    attr_reader :alignments
+    
+    def initialize(io)
+      raise EncodingError, "Not delta format" unless format_ok?(io)
+      @alignments = parse(io.read)
+    end
+
+    def parse(string)
+      string.split("\n").slice_before(/^>/).flat_map do |block|
+        refname, queryname = block.shift.match(/>(.*) (.*) \d+ \d+/).captures
+        block.slice_before(/\d+ \d+ \d+ \d+/).map do |alignment|
+          refstart, refstop, querystart, querystop = alignment
+            .shift
+            .match(/(\d+) (\d+) (\d+) (\d+) /)
+            .captures
+            .map{ |c| c.to_i }
+          alignment.pop
+          Alignment.new(refname,
+                        queryname,
+                        refstart,
+                        refstop,
+                        [querystart, querystop].min,
+                        [querystart, querystop].max,
+                        querystart < querystop,
+                        alignment.map{ |i| i.to_i })
+        end
+      end
+    end
+
+    def format_ok?(io)
+      line = io.gets.chomp
+      unless line.match(/^(\/.*) (\/.*)$/)
+        return false
+      else
+        reference_filename = $1
+        alternate_filename = $2
+      end
+
+      case io.gets.chomp
+      when /NUCMER/
+        @format = NUCMER
+        return true
+      when /PROMER/
+        @format = PROMER
+        return true
+      else
+        return false
+      end
+    end
+
+    def transpose_region(refname, startpos, endpos)
+      a = alignments.find do |a|
+        a.refname == refname && a.refstart <= startpos && a.refstop >= endpos
+      end
+      if a
+        querystart = a.ref_to_query(startpos)
+        querystop = a.ref_to_query(endpos)
+        if querystart.nil? || querystop.nil?
+          return nil
+        elsif a.strand
+          return [querystart, querystop].join("...")
+        else
+          return [querystop, querystart].join("...")
+        end
+      else
+        return nil
+      end
+    end
+
+    def self.open(filename)
+      return new(File.open(filename, 'r'))
+    end
+  end
 end
--- test/test_bio-mummer.rb
+++ test/test_bio-mummer.rb
@@ -1,8 +1,42 @@
 require 'helper'
 
 class TestBioMummer < MiniTest::Test
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  def setup
+    @report = BioMummer::DeltaFile.open("test/data/out.delta")
+  end
+  
+  should "open and close a delta file" do
+    assert_kind_of BioMummer::DeltaFile, @report
+  end
+
+  should "have enumerable alignments" do
+    assert_equal "scaffold_012", @report.alignments.first.refname
+    assert_kind_of Fixnum, @report.alignments.first.refstart
+  end
+
+  should "do basic coordinate transforms on the positive strand" do
+    a = BioMummer::Alignment.new('ref', 'query', 101, 150, 201, 248, true, [5,-2,3,1,1,-4])
+    assert_equal 201, a.ref_to_query(101)
+    assert_equal 202, a.ref_to_query(102)
+    assert_equal 203, a.ref_to_query(103)
+    assert_equal 204, a.ref_to_query(104)
+    assert_equal nil, a.ref_to_query(105)
+    assert_equal 205, a.ref_to_query(106)
+    assert_equal 207, a.ref_to_query(107)
+    assert_equal 208, a.ref_to_query(108)
+    assert_equal nil, a.ref_to_query(109)
+    assert_equal nil, a.ref_to_query(110)
+    assert_equal nil, a.ref_to_query(111)
+    assert_equal 209, a.ref_to_query(112)
+    assert_equal 217, a.ref_to_query(119)
+    assert_equal 248, a.ref_to_query(150)
+  end
+
+  should "do basic coordinate transforms on the negative strand" do
+    a = BioMummer::Alignment.new('ref', 'query', 101, 118, 201, 216, false, [5,-2,3,1,1,-4])
+    assert_equal 216, a.ref_to_query(101)
+    assert_equal 201, a.ref_to_query(118)
   end
 end
 
